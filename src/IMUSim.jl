92e7389b631742889acd8e9a4336f4cd8443a2d2
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-04-03 13:15:03 +0200

    feat: type system

--- src/IMUSim.jl
+++ src/IMUSim.jl
@@ -1,19 +1,18 @@
 module IMUSim
 
-function generatePositionPath(positionFn::Function{Float64}, range::Tuple{Float64,Float64,Float64})
-    (rangeFrom, rangePrecision, rangeTo) = range
-    movementPieces = floor((rangeTo - rangeFrom) / rangePrecision)
-    return zeros(movementPieces, 6)
+struct PositionPath
+    posXYZrotXYZ::Matrix{Float64}
+    PositionPath(positionFn::Function, length::Int) = new(zeros(length, 6))
 end
 
-function getPositionXYZAt(point::Float64)
+function getPositionXYZAt(path::PositionPath, t::Int)::Vector{Float64}
     return [0, 0, 0]
 end
 
-function getRotationXYZAt(point::Float64)
+function getRotationXYZAt(path::PositionPath, t::Int)::Vector{Float64}
     return [0, 0, 0]
 end
 
-export generatePositionPath, getPositionXYZAt, getRotationXYZAt
+export PositionPath, getPositionXYZAt, getRotationXYZAt
 
 end # module
--- src/movement.test.jl
+++ src/movement.test.jl
@@ -3,7 +3,7 @@ include("IMUSim.jl")
 
 @testset "uniform rectilinear motion" begin
     rectilinear(t) = [2 * t, 0, 0, 0, 0, 0] # 2m/s
-    positionPath = IMUSim.generatePositionPath(rectilinear, [0, 0.1, 100])
+    positionPath = IMUSim.PositionPath(rectilinear, 100)
     @test IMUSim.getPositionXYZAt(positionPath, 0) == [0, 0, 0]
     @test IMUSim.getRotationXYZAt(positionPath, 0) == [0, 0, 0]
     @test IMUSim.getPositionXYZAt(positionPath, 5) == [2 * 5, 0, 0]
