eb9fccd5d2019359b75384b94071d5f05c81d69f
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-04-04 14:25:20 +0200

    test: accelerated motion

--- src/IMUSim.test.jl
+++ src/IMUSim.test.jl
@@ -8,3 +8,10 @@ include("IMUSim.jl")
     @test positionPath.posXYZrotXYZ[2, :] == [4, 0, 0, 0, 0, 0]
     @test positionPath.posXYZrotXYZ[3, :] == [6, 0, 0, 0, 0, 0]
 end
+
+@testset "MCUPath constructor" begin
+    rectilinear(t) = [2 * t ^ 2, 0, 0, 0, 0, 0] # 2m/s
+    positionPath = IMUSim.PositionPath(rectilinear, 100)
+    mcuReadingsPath = IMUSim.MCUPath(positionPath)
+    @test mcuReadingsPath.accXYZgyrXYZ[5, :] == [4, 0, 0, 0, 0, 0]
+end
--- src/movement.test.jl
+++ src/movement.test.jl
@@ -9,3 +9,10 @@ include("IMUSim.jl")
     @test IMUSim.getPositionXYZAt(positionPath, 5) == [2 * 5, 0, 0]
     @test IMUSim.getRotationXYZAt(positionPath, 5) == [0, 0, 0]
 end
+
+@testset "uniformly accelerated motion with gravity" begin
+    accelerated(t) = [0, 0, 2 * t ^ 2, 0, t ^ 2, 0] # 2m/s^2
+    mcuReadingsPath = IMUSim.MCUPath(IMUSim.PositionPath(accelerated, 100))
+    @test IMUSim.getAccelerationXYZAt(mcuReadingsPath, 10) == [0, 0, 4]
+    @test IMUSim.getAngularRateXYZAt(mcuReadingsPath, 10) == [0, 2, 0]
+end
