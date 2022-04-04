213bceee972e231daac2c68a8ad71a1519306217
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-04-04 14:02:44 +0200

    fix: kernel padding and minus signs of derivation matrix

--- src/euler.jl
+++ src/euler.jl
@@ -1,26 +1,28 @@
 function eulerDerivativeOfMatrix(matrixRowBased::Matrix{Float64}; order=1)
-    kernelVec = pascalSubtractionTriangleCoeffsAt(order + 1)
+    kernelVec = pascalSubtractionTriangleCoeffsAt(order + 1)'
     kernelSize = size(kernelVec)[2]
 
     derivedM = zeros(size(matrixRowBased))
     rowCount = size(matrixRowBased)[1]
     colCount = size(matrixRowBased)[2]
     for i = 1:rowCount
-        kernelView::Metrix{Float64}
-        if i < kernelSize
-            paddingSize = kernelSize - i
-            paddingM = zeros(paddingSize, colCount)
-            kernelView = [paddingM; matrixRowBased[i:kernelSize, :]]
-        else
-            viewStart = i - kernelSize
-            viewEnd = i
-            kernelView = matrixRowBased[viewStart:viewEnd, :]
-        end
-        derivedRow = kernelVec * kernelView
+        viewRangeIndices = (i - kernelSize + 1, i)
+        derivedRow = kernelVec * paddedMatrixView(matrixRowBased; indices=viewRangeIndices, columnCount=colCount)
         derivedM[i, :] = derivedRow
     end
 
     return derivedM
+end
+
+function paddedMatrixView(matrixRowBased::Matrix{Float64}; indices::Tuple{Int,Int}, columnCount::Int)::Matrix{Float64}
+    (s, e) = indices
+    if s <= 0
+        paddingSize = -s + 1
+        paddingM = zeros(paddingSize, columnCount)
+        return [paddingM; matrixRowBased[1:e, :]]
+    else
+        return matrixRowBased[s:e, :]
+    end
 end
 
 function pascalSubtractionTriangleCoeffsAt(n::Int)::Vector{Int}
@@ -28,5 +30,5 @@ function pascalSubtractionTriangleCoeffsAt(n::Int)::Vector{Int}
     for i = 0:n-1
         coeffs[i+1] = binomial(n - 1, i) * (i % 2 == 0 ? 1 : -1)
     end
-    return coeffs
+    return reverse(coeffs)
 end
