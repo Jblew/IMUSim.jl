3d28197d32da6bd1e6f7754760408b64fcada8a6
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-04-04 09:42:19 +0200

    feat: arbitrary order euler derivative kernel

--- src/euler.jl
+++ src/euler.jl
@@ -1,9 +1,19 @@
-function euler2nOrderDerivative(matrixRowBased::Matrix{Float64}; order=1)
-    2nOrderDerivativeKernel = [
-        fill(1, 6)'
-        fill(-2, 6)'
-        fill(1, 6)'
-    ]
+function eulerDerivative(matrixRowBased::Matrix{Float64}; order=1)
+    kernel = getDerivativeKernel(; order, kernelLength=6)
     # Can we apply this in loop that will be multithreaded?
     return m
+end
+
+function getDerivativeKernel(; order::Int, kernelLength::Int)
+    coeffs = pascalSubtractionTriangleCoeffsAt(order + 1)
+    kernelLines = [fill(coeffs[i], kernelLength)' for i in 1:length(coeffs)]
+    return vcat(kernelLines...)
+end
+
+function pascalSubtractionTriangleCoeffsAt(n::Int)::Vector{Int}
+    coeffs::Vector{Int} = zeros(n)
+    for i = 0:n-1
+        coeffs[i+1] = binomial(n - 1, i) * (i % 2 == 0 ? 1 : -1)
+    end
+    return coeffs
 end
